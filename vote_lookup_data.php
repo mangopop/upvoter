839752ee64bc797898bfd259fa8bebc81461b567
vagrant <simon.norton@gmail.com>
2014-10-18 18:32:43 +0000

    state before array key

---
+++ vote_lookup_data.php
@@ -0,0 +1,66 @@
+<?php
+
+use \core\vote_lookup;
+use \core\Tasks\Task;
+
+require_once 'app/startup.php';
+$helper = new \core\Helpers();
+
+$final_array = array();
+
+//get all votes for tasks
+//$votes = vote_lookup::find('all');
+$lookup_data = vote_lookup::find_by_sql('SELECT tasks.task, v.votes, v.user_id FROM vote_lookups as v LEFT JOIN tasks ON v.task_id = tasks.id');
+$votes_left = vote_lookup::find('all', array('select' => 'votes'), array('conditions' => array('user_id = ?', 1)));
+//$join = 'LEFT JOIN tasks ON (vote_lookups.task_id = tasks.id)';
+//$lookup = vote_lookup::all(array('joins' => $join));
+
+//echo $votes_left[0]->votes;
+//$helper->pre($votes_left);
+
+//1.get task name - join from vote_lookup (will repeat - filter unique?)
+//2.get total votes - select from vote_lookup (count votes where task_id)
+//3.get user votes for task - select from vote_lookup (count votes where task_id and user_id)
+//4.get votes left - select from vote_lookup (count votes where user_id)
+
+//if the sql result array returns everything the can calculate from that an and return in one!
+
+//echo $data[0]->task;
+
+//$helper->pre($tasks);
+
+// get names from tasks = task name / final-array   DONE
+// go through each array                            DONE
+// add up votes where name matches = total-votes    DONE
+// add those to task final-array                    DONE
+// add votes where user_id matches = user-votes     DONE
+// take above and subtract from 5 = votes-left
+
+//$helper->pre($lookup_data);
+
+$tasks = Task::find('all');//array-object
+foreach ($tasks as $task) {
+    $final_array[$task->task] = array(
+        'votes' =>'',
+        'user_votes' =>''
+    );
+}
+
+
+foreach ($final_array as $key => $val1) { //key = name, val1 = empty
+
+    foreach ($lookup_data as $val2) { //task, votes,user_id
+        // add up votes where name matches $key
+        if($key === $val2->task){
+            $final_array[$key]['votes'] += $val2->votes;
+            // add votes where user_id matches ... users id
+            if($val2->user_id === 1){
+                $final_array[$key]['user_votes'] = $val2->votes;
+            }
+        }
+    }
+}
+
+//$helper->pre($final_array);
+//echo $helper->arToJson($final_array);
+echo json_encode($final_array);
